6aa1604237bfecf211fbbe8822428bae6d5d7e74
xuch2 <wwts@nate.com>
2019-10-24 21:47:12 +0900

    Java 9th

--- src/QuizAns4ArrayVersion.java
+++ src/QuizAns4ArrayVersion.java
@@ -12,17 +12,29 @@ public class QuizAns4ArrayVersion {
        합치기 결과를 출력하시오.
      */
     public static void main(String[] args) {
+        // 이름이 series 인 int 형 배열을 선언!
         int[] series;
         Scanner in = new Scanner(System.in);
         System.out.println("몇 번째 항까지 합칠까요 ? ");
         int num = in.nextInt();
 
+        // 실제 [num] 개수만큼 int 형 배열을 할당(메모리 할당)
         series = new int[num];
 
+        // 메모리가 할당되었으므로 실제 데이터를 배치
         series[0] = 1;
         series[1] = 1;
         series[2] = 3;
 
+        /* 1 1 3 4 5 8 12 17 25 37 ...
+           1 번째와 3 번째를 더해서 4 번째가 갱신됨
+           2 번째와 4 번째를 더해서 5 번째가 갱신됨
+           3 번째와 5 번째를 더해서 6 번째가 갱신됨
+
+           배열의 인덱스는 0 부터 시작한다는 것!
+           그로므로 1 번째는 [0], 2 번째는 [1], 3 번째는 [2],
+           4 번째는 [3], 5 번째는 [4], 6 번째는 [5] 를 기억하자!
+         */
         for(int i = 3; i < num; i++) {
             series[i] = series[i - 3] + series[i - 1];
         }
