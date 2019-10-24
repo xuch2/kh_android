85f66abe9d2ba097aba2ae56c5c3b76b36d79049
xuch2 <56873196+xuch2@users.noreply.github.com>
2019-10-24 19:10:22 +0900

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Java with Git");
+        System.out.println("Additional Edit");
+        System.out.println("Git Pull Test");
     }
 }
