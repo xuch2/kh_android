eee5dc189dd4d5d1401a8f4cdd71202ea5e43dba
xuch2 <wwts@nate.com>
2019-10-25 20:47:48 +0900

    Java 10th

--- src/ChallengeProb.java
+++ src/ChallengeProb.java
@@ -7,7 +7,7 @@ public class ChallengeProb {
 
        위와 같은 데이터가 배열에 들어있다고 가정한다(무선 주파수임)
        여기서 가장 빈도수가 높은 주파수 대역을 2 개 추출하고
-       몇 번 검출되었는지 확인하는 프로그램을 작성하시오.
+       몇 번 검출되었는지 확인하는 프로그램을 작성하시오. -삼성 네트워크 7년차 문제-
 
        1. 목적: 가장 빈번한 숫자 2 개와 출현 횟수
        2. 배열을 활용하도록 한다.
@@ -107,5 +107,107 @@ public class ChallengeProb {
         }
 
         System.out.println("중복 없는 원소의 개수는 = " + nonRecursNum);
+
+        number = new int[nonRecursNum];
+        freqCheck = new int[nonRecursNum];
+
+        int nonRecursElem = 0;
+
+        for(i = 0; i < len; i++) {
+            for(j = 0; j < i; j++) {
+                if(wirelessFreq[i] == wirelessFreq[j]) {
+                    break;
+                }
+            }
+
+            if(j == i) {
+                number[nonRecursElem++] = wirelessFreq[i];
+            }
+        }
+
+        System.out.println("nonRecursElem = " + nonRecursElem);
+
+        for(i = 0; i < nonRecursElem; i++) {
+            System.out.println("number[" + i + "] = " + number[i]);
+        }
+
+        /* 빈도수에 대한 검사는
+           기존에 중복없는 숫자 배열을 만드는 것과 동일한 패턴이다.
+           다만 중복되었을 때 이 녀석이
+           어디에 배치되어야 하는지를 한 번 더 생각해줘야 한다.
+
+           아래 방법 보다 더 빠르고 효율적인 방법
+
+           i Loop 를 len 으로 검색하는게 아니ㅣ라
+           nonRecursElem 으로 for Loop 을 돌리고
+           wirelessFreq 전체를 비교해서 값을 올리면 훨씬 간단하다.
+
+           ### Loop 란 ? ###
+           반복문을 의미한다.
+           while Loop, for Loop 등등
+         */
+
+        for(i = 0; i < len; i++) {
+            for(j = 0; j < nonRecursElem; j++) {
+                if(number[j] == wirelessFreq[i]) {
+                    freqCheck[j]++;
+                    break;
+                }
+            }
+        }
+
+        /*
+        for(i = 0; i < len; i++) {
+           for(j = 0; j < i; j++) {
+               if(wirelessFreq[i] == wirelessFreq[j]) {
+                   for(int k = 0; k < nonRecursElem; k++) {
+                       if(wirelessFreq[i] == number[k]) {
+                           freqCheck[k]++;
+                           break;
+                       }
+                   }
+               }
+           }
+
+            // 중복은 없다가 보장되는 부분임
+            if(i == j) {
+                for(int k = 0; k < nonRecursElem; k++) {
+                    if(wirelessFreq[i] == number[k]) {
+                        freqCheck[k] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+         */
+
+        for(i = 0; i < nonRecursElem; i++) {
+            System.out.println("freqCheck[" + i + "] = " +
+                    freqCheck[i]);
+        }
+
+        /* 7. 오름 차순으로 freqCheck 와 number 를 정렬하면 끝!
+              내림 차순 버그는 월요일날 해결해서 코드 제공!
+         */
+        int key2;
+
+        for(i = 1; i < nonRecursNum; i++) {
+            key = freqCheck[i];
+            key2 = number[i];
+
+            for(j = i - 1; freqCheck[j] > key; j--) {
+                freqCheck[j + 1] = freqCheck[j];
+                number[j + 1] = number[j];
+            }
+
+            freqCheck[j + 1] = key;
+            number[j + 1] = key2;
+        }
+
+        for(i = 0; i < nonRecursNum; i++) {
+            System.out.println("number[" + i + "] = " +
+                                number[i] + ", freqCheck[" + i +
+                                "] = " + freqCheck[i]);
+        }
     }
 }
